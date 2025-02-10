fd1674898c04552cfef358e0b93ec263f31d423d
Luna Heyman <luna.heyman@proton.me>
2025-02-10 17:44:20 +0100

    build: initialize nix flake

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "My Nix flake containing system configurations, packages, utilities and more!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: {
+  };
+}

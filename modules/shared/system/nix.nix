be2845cfd4711625090612490dbd16be531973c6
Luna Heyman <luna.heyman@proton.me>
2025-02-12 19:10:38 +0100

    feat: add extra nix settings

--- modules/shared/system/nix.nix
+++ modules/shared/system/nix.nix
@@ -1,7 +1,26 @@
+{ pkgs, lib, ... }:
+let
+  sudoers = lib.soul.systems.ternary pkgs "@wheel" "@admin";
+in
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-    "pipe-operator"
-  ];
+  nix = {
+    channel.enable = false;
+    optimise.automatic = true;
+    gc.automatic = pkgs.stdenv.hostPlatform.isDarwin;
+
+    settings = {
+      keep-going = true;
+      warn-dirty = false;
+      use-xdg-base-directories = true;
+
+      allowed-users = [ sudoers ];
+      trusted-users = [ sudoers ];
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operator"
+      ];
+    };
+  };
 }

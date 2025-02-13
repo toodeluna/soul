db4168c8035b71108273342298270c588e35a53e
Luna Heyman <luna.heyman@proton.me>
2025-02-13 16:01:31 +0100

    fix(hosts/blackstar): make a vps configuration that actually works

--- hosts/blackstar/default.nix
+++ hosts/blackstar/default.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 {
-  imports = [ ./disks.nix ];
+  imports = [ ./disks.nix ./hardware.nix ];
 
   system.stateVersion = "24.11";
   networking.hostName = "blackstar";
+  services.openssh.enable = true;
 
   soul = {
     users.me = {
@@ -12,11 +13,20 @@
     };
   };
 
+  users.users.root = {
+    hashedPassword = "$y$j9T$mRU04ULTVP3VcjRv6w0Mh/$FHBN/V5MRcirNQx9pLpI.ISk7GSCbFWEedOoyRdoCz5";
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxCvGO9OqARsPJl/bKRumMHC/zFgRyFLEVQrru/z7qr luna@excalibur"
+    ];
+  };
+
   users.groups.luna = {};
 
   users.users.luna = {
     hashedPassword = "$y$j9T$mRU04ULTVP3VcjRv6w0Mh/$FHBN/V5MRcirNQx9pLpI.ISk7GSCbFWEedOoyRdoCz5";
     group = "luna";
+    extraGroups = [ "wheel" ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxCvGO9OqARsPJl/bKRumMHC/zFgRyFLEVQrru/z7qr luna@excalibur"

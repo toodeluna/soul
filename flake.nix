46e9cf719d6374465f4c909ad6636379e2f66b32
Luna Heyman <luna.heyman@proton.me>
2025-02-25 17:09:41 +0100

    build: add flake-parts input

--- flake.nix
+++ flake.nix
@@ -3,8 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs: {
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
   };
 }
